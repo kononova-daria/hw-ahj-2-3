d9609020ff7595d655e051e58cc4216f09c4bdc5
Kononova Daria <kononova.yu.daria@mail.ru>
2022-01-04 21:57:30 +0300

    123

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ let tickets = [
 app.use(async (ctx) => {
   let method;
   if (ctx.request.method === 'GET' || ctx.request.method === 'DELETE') ({ method } = ctx.request.query);
-  if (ctx.request.method === 'POST' || ctx.request.method === 'PUT') ({ method } = ctx.request.body);
+  if (ctx.request.method === 'POST' || ctx.request.method === 'PUT') ({ method } = ctx.request.query);
 
-  let ticket;
+  // let ticket;
 
   switch (method) {
     case 'allTickets':
